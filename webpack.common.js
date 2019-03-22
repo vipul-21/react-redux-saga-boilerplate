ae417cf4d245752dc2916a7b7845224c73e60fe7
vipul <vipul21sept@gmail.com>
2019-03-22 02:11:00 -0700

    Added linter, babel and common webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    js: ['babel-polyfill', './src/index'],
+  },
+  context: __dirname,
+  target: 'web',
+  stats: 'errors-only',
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            publicPath: 'https://new-admin-tools-staging.s3.amazonaws.com/',
+          },
+
+        },
+      ],
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', 'css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+};
